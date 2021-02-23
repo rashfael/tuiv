8a15a81a11da7b0540209944828918aedd0ff68c
rash <sebastian.gepperth@ax-semantics.com>
2021-02-23 21:36:30 +0100

    always clone chain for branching to actually work

--- src/ChainingProxy.js
+++ src/ChainingProxy.js
@@ -1,5 +1,3 @@
-const cloneDeep = require('lodash/cloneDeep')
-
 module.exports = function (chain, target, handler) {
 	if (!chain) chain = {
 		selectors: [],
@@ -7,15 +5,14 @@ module.exports = function (chain, target, handler) {
 		elementPromise: null,
 		element: null,
 	}
-	let visited = false
 	const handleWrapper = {}
 	for (const key of Reflect.ownKeys(handler)) {
 		handleWrapper[key] = function () {
-			if (visited) {
-				chain = cloneDeep(chain)
-			}
-			visited = true
-			return handler[key].call(this, chain, ...arguments)
+			// be careful not to deep clone element
+			const clonedChain = Object.assign({}, chain)
+			clonedChain.selectors = chain.selectors.slice()
+			clonedChain.frames = chain.frames.slice()
+			return handler[key].call(this, clonedChain, ...arguments)
 		}
 	}
 	return new Proxy(target, handleWrapper)
