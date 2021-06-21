ffe3ff81b3e9bc754be700d5711eb79027252814
rash <sebastian.gepperth@ax-semantics.com>
2021-06-21 22:14:39 +0200

    handle Array.forEach ourselves and start a chain for each item

--- src/chain/handlers/array.js
+++ src/chain/handlers/array.js
@@ -1,5 +1,5 @@
 const defaultHandler = require('./default')
-
+const ChainingProxy = require('../ChainingProxy')
 module.exports = async function (array, op, {chain, meta, assertion}) {
 	meta.valueSelectors.push(op.get)
 	if (op.get === 'first') {
@@ -7,6 +7,16 @@ module.exports = async function (array, op, {chain, meta, assertion}) {
 	}
 	if (op.get === 'last') {
 		return array[array.length - 1]
+	}
+	if (op.get === 'forEach') {
+		// rewrap each array item into a new chain
+		// and execute in series
+		const fn = op.apply[0]
+		for (let i = 0; i < array.length; i++) {
+			// TODO merge chain for better error messages
+			await fn(new ChainingProxy(array[i]), i)
+		}
+		return
 	}
 
 	return defaultHandler(array, op)
